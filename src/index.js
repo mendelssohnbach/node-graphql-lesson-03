ee318734f6fb50e715909778d0153480f05e51ea
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2022-07-23 00:57:45 +0900

    :bug: PORT -> port

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const { ApolloServer } = require('apollo-server');
 const { typeDefs } = require('./schema');
 const { resolvers } = require('./resolvers');
 
-const PORT = process.env.PORT || 9090;
+const port = process.env.PORT || 9090;
 
 const server = new ApolloServer({ resolvers, typeDefs });
 
-server.listen({ PORT }, () => console.log(`Server runs at: http://localhost:${PORT}`));
+server.listen({ port }, () => console.log(`Server runs at: http://localhost:${port}`));
